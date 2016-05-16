d89469f926694956355bf947a09102baa5b800ed
Trent Albright <trent.albright@gmail.com>
2016-05-16 14:24:44 -0400

    Provide deep copy for Znodes

--- znode_test.go
+++ znode_test.go
@@ -2,6 +2,7 @@ package curator_test
 
 import (
 	. "github.com/talbright/go-curator"
+	"github.com/talbright/go-zookeeper/zk"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -26,6 +27,23 @@ var _ = Describe("Znode", func() {
 	Context("Parent", func() {
 		It("should extract the parent name of the node", func() {
 			Expect(node.Parent()).Should(Equal("/usr/local/var"))
+		})
+	})
+	Context("DeepCopy", func() {
+		It("should create a deep copy of the znode with nil data and stat", func() {
+			znodeCopy := node.DeepCopy()
+			Expect(znodeCopy.Data).Should(BeNil())
+			Expect(znodeCopy.Stat).Should(BeNil())
+		})
+		It("should create a deep copy of the znode with data and stat", func() {
+			node.Stat = &zk.Stat{Version: 123}
+			node.Data = []byte("abc")
+			znodeCopy := node.DeepCopy()
+			Expect(znodeCopy.Stat == node.Stat).ShouldNot(BeTrue())
+			node.Stat.Version = 99
+			Expect(znodeCopy.Stat.Version).ShouldNot(Equal(node.Stat.Version))
+			node.Data[0] = 'z'
+			Expect(string(znodeCopy.Data[:])).ShouldNot(Equal(string(node.Data[:])))
 		})
 	})
 })
